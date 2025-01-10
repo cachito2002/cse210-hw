8346eb109402a72a93d4ffa56638ae78ffc3b4e1
cachito2002 <fer21012@byui.edu>
2025-01-09 18:40:32 -0700

    made adjustments to the code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World! Hows it going");
+        Console.WriteLine("Hello Sandbox World!");
     }
 }
