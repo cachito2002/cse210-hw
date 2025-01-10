be90e17042148b605df74a0357e7df1f1186b296
cachito2002 <fer21012@byui.edu>
2025-01-09 18:55:39 -0700

    I made some changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2.0
+        // This is prep 2.0 Hows it going dude
         Console.WriteLine("Hello Prep2 World!");
     }
 }
