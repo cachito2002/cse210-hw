86eeae8bfc404d33fb2e5b73c5ea2736d40e58e6
cachito2002 <fer21012@byui.edu>
2025-01-10 17:25:42 -0700

    bruh

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2.0 Hows it going dude
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("What is your Grade percentage? ");
+       string gradePercentage = Console.ReadLine();
+       string letter;
+       int x = int.Parse(gradePercentage);
+
+    
+
+       if (x >= 90)
+       {
+            letter = "A";
+            Console.WriteLine($"Grade: {letter}");
+       }
+       else if (x >= 80)
+       {
+            letter = "B";
+            Console.Write($"{letter}");
+       }
+       else if (x >= 70)
+       {
+            letter = "C";
+            Console.Write($"{letter}");
+       }
+       else if (x >= 60)
+       {
+            letter = "D";
+            Console.Write($"{letter}");
+       }
+       else
+       {
+            letter = "F";
+            Console.WriteLine($"{letter}");
+       }
+
+       if (x >= 70 )
+       {
+            Console.WriteLine("You did amazing this semester, you passed the class! See you next time!!");
+       }
+       else
+       {
+            Console.WriteLine("Good Luck next time dude, you did not pass, you will have to retake the class!!!");
+       }
+       
+
+
+
+    
+
+        
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,18 +1,39 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World! bruh");
-         Console.WriteLine("What is up goody");
+        // Console.WriteLine("Hello Sandbox World! bruh");
+        // Console.WriteLine("What is up goody");
 
-        int number = 5;
-        number = 8;
-        number = number + 3;
+        // int number = 5;
+        // number = 8;
+        // number = number + 3;
         
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
-        Console.WriteLine($"Your color is {color}");
+        // Console.Write("What is your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"Your color is {color}");
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+        int y = 2;
+        int z = 5;
+        if (x > y && y < z)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x != y)
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("Bruh non of the above");
+        }
+
     }
 }
