e1953427c2ba6a3c0e0523cf38ee820f2ea1f15b
cachito2002 <fer21012@byui.edu>
2025-01-14 16:54:35 -0700

    ye

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,6 +6,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        string response = "yes";
+        while (response == "yes");
+
+        {
+            Console.Write("Do you want to continue? ");
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Console.WriteLine("Hello Sandbox World! bruh");
         // Console.WriteLine("What is up goody");
 
@@ -16,24 +44,24 @@ class Program
         // Console.Write("What is your favorite color? ");
         // string color = Console.ReadLine();
         // Console.WriteLine($"Your color is {color}");
-        Console.Write("Enter number: ");
-        string valueFromUser = Console.ReadLine();
+        // Console.Write("Enter number: ");
+        // string valueFromUser = Console.ReadLine();
 
-        int x = int.Parse(valueFromUser);
-        int y = 2;
-        int z = 5;
-        if (x > y && y < z)
-        {
-            Console.WriteLine("Greater");
-        }
-        else if (x != y)
-        {
-            Console.WriteLine("Less");
-        }
-        else
-        {
-            Console.WriteLine("Bruh non of the above");
-        }
+        // int x = int.Parse(valueFromUser);
+        // int y = 2;
+        // int z = 5;
+        // if (x > y && y < z)
+        // {
+        //     Console.WriteLine("Greater");
+        // }
+        // else if (x != y)
+        // {
+        //     Console.WriteLine("Less");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Bruh non of the above");
+        // }
 
     }
 }
