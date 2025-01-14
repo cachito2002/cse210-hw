f375d72838f9925a6a8e76cec54e546d59b3288f
cachito2002 <fer21012@byui.edu>
2025-01-14 16:55:27 -0700

    ye

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         string response = "yes";
-        while (response == "yes");
+        while (response == "yes")
 
         {
             Console.Write("Do you want to continue? ");
