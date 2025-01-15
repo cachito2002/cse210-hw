0f2891974a01c3a96ac7493275b03d6ff55d386f
cachito2002 <fer21012@byui.edu>
2025-01-15 12:45:55 -0700

    bruh

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //    This is the main section
+        Console.WriteLine("PREP5");
+        DisplayMessage();
+        string name = AskName("Please enter your name: ");
+        int number = AskNumber("Please enter your favorite number: ");
+        EndResult();
+
     }
+    //    These are the functions
+    static void DisplayMessage()
+    {
+    Console.WriteLine($"Hello World");
+    }
+    
+    static string AskName (string prompt)
+    {
+        Console.Write(prompt);
+        string userInput = Console.ReadLine();
+        return userInput;
+    }
+
+    static int AskNumber(string prompt)
+    {
+        Console.Write(prompt);
+        string userInput = Console.ReadLine();
+        int userNumber = Convert.ToInt32(userInput);
+        return userNumber;
+
+    static void EndResult(string name, int number)
+    {
+    int squared =  number * number;
+    Console.WriteLine($"{name}, the square of your number is {squared}.");
+    }
+
 }
